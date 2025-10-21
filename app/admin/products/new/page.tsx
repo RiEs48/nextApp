2a69dd7f8d27d96ae3101da88bd72617b73f4107
Ries48 <rivas.e2.marvin@gmail.com>
2025-10-21 01:01:35 -0400

    Creando una Composicion en Nextjs con Children

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,11 +1,15 @@
 import AddProductForm from "@/components/productos/AddProductForm";
+import ProductForm from "@/components/productos/ProductForm";
 import Titulo from "@/components/ui/Titulo";
 
 export default function CreateProductPage() {
   return (
     <>
       <Titulo>Nuevo Producto</Titulo>
-      <AddProductForm />
+
+      <AddProductForm>
+        <ProductForm />
+      </AddProductForm>
     </>
   );
 }
--- components/productos/AddProductForm.tsx
+++ components/productos/AddProductForm.tsx
@@ -1,11 +1,18 @@
-import ProductForm from "./ProductForm";
+"use client";
 
-export default function AddProductForm() {
+import type React from "react";
+
+export default function AddProductForm({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const handleSubmit = async (formData: FormData) => {};
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
       <form className="space-y-5" action={handleSubmit}>
-        <ProductForm />
+        {/* lo que le pasemos aqui puede ser un Compnonete de Servidor o de Cliente  children*/}
+        {children}
         <input
           type="submit"
           className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
