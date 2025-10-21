153ca2576cdce52876c94f7f4a5a52381834eb90
Ries48 <rivas.e2.marvin@gmail.com>
2025-10-21 00:56:04 -0400

    Creando los Componentesy formularios  de CrearProductos

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,3 +1,11 @@
-export default function page() {
-  return <div>page</div>;
+import AddProductForm from "@/components/productos/AddProductForm";
+import Titulo from "@/components/ui/Titulo";
+
+export default function CreateProductPage() {
+  return (
+    <>
+      <Titulo>Nuevo Producto</Titulo>
+      <AddProductForm />
+    </>
+  );
 }
